0dcf93ca1a2af6cbdb447871fd80abce931c1312
ahmadcs22 <ahmadsirajsuperior@gmail.com>
2026-03-31 02:38:01 +0500

    list

---
+++ list.py
@@ -0,0 +1,53 @@
+# #multiple data types
+# fruits=['apple','banana','grapes','peach','watermelon','strawberry']
+
+
+# if 'apple' in fruits:
+#     print('fruit found')
+# else:
+#     print('not found ')
+
+
+# student=['ahmad','ali','ayehsa','maryam']
+# student.insert(3,'hashim')
+# print(student)
+
+
+
+# student=['ahmad','ali','ayehsa','maryam']
+
+# tustd=('ayeza','abdullah')
+# morestd=['hammad','faheem','moazam']
+# # student.append('hadi')
+# student.extend(tustd)
+
+
+
+# student.pop(0)
+# student.remove('ali')
+# del student[0]
+# student.clear()
+
+
+
+
+# for i in student:
+#     print(i)
+
+
+# for i in range(len(student)):
+#     print(student[i])
+
+student=['ahmad','ali','ayehsa','Maryam']
+
+# i=0
+# while i < len(student):
+#     print(student[i])
+#     i=i+1
+
+# [print(x) for x in student]
+
+num=[3,5,7,2,7,34,87,243,87,2345,875,4]
+student.sort()
+print(student)
+

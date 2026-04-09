f7ebb77578204bdbfc6bb8517505a47ebf13da37
ahmadcs22 <ahmadsirajsuperior@gmail.com>
2026-04-10 02:00:29 +0500

    dictionary

---
+++ dict.py
@@ -0,0 +1,38 @@
+# student={
+#     'name': 'ahmad',
+#     'roll': 97,
+#     'grade': True
+# }
+
+# print(student['roll'])
+
+# students={
+# 'ahmad':{'roll':97,'grades':True},
+# 'ali':{'roll':99,'grades':True},
+# }
+
+student={
+    'name': 'ahmad',
+    'roll': 97,
+    'grade': True
+}
+
+# student['name']='hania'
+# student.update({'roll':100})
+
+# student['city']='grw'
+# student.update({'dept':'cs'})
+
+# student.pop('grade')
+
+# student.popitem()
+
+# del student['roll']
+
+# student.clear()
+
+
+
+
+print(student.items())
+

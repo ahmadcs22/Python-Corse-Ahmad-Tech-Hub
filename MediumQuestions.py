1e491bbccf9a9a337d84936f18af1d9c02b73035
ahmadcs22 <ahmadsirajsuperior@gmail.com>
2026-04-10 23:19:23 +0500

    added 10 questions

--- MediumQuestions.py
+++ MediumQuestions.py
@@ -38,3 +38,56 @@
 #         print("Buzz")
 #     else:
 #         print(i)
+# 24.	Word Frequency: Given a sentence, count how many times each specific word appears using lists and sets
+# sentence = 'Ahmad is a good boy and boys like ahmad are very good and hardworking a a '
+
+# words = sentence.lower().split()
+# unique_words=set(words)
+# for i in unique_words:
+#     print(f'{i} count is : {words.count(i)}')
+
+# 25.	The Common Denominator: Find all numbers between 100 and 500 that are perfectly divisible by both 4 and 7.
+# num=[]
+# for i in range(100,501):
+#     if i%4==0 and i%7==0:
+#         num.append(i)
+# print(num)
+
+# 26.	List Merger: Take two sorted lists and merge them into a single, correctly sorted list.
+
+# l1=[1,2,3,4,5,6,7,8,9]
+# l2=[4,5,7,8,3,4,5,2,1]
+# l1.extend(l2)
+# l1.sort()
+# print(l1)
+
+# 27.	Password Validator: Write a script that checks a string to ensure it has at least 8 characters, contains at least one number, and one uppercase letter.
+
+# password = input('Enter Your Password = ')
+
+# if len(password)>=8 and any(word.isdigit() for word in password) and any(word.isupper() for word in password):
+#     print('valid')
+# else:
+#     print('invalid')
+
+# 28.	Matrix Flattener: You have a 2D list (e.g., [[1, 2], [3, 4]]). Use nested for loops to flatten it into a 1D list ([1, 2, 3, 4]).
+# new=[]
+# for i in matrix:
+#     for m in i:
+#         new.append(m)
+
+# print(new)
+# matrix = [[1, 2], [3, 4]]
+# matrix=[m for item in matrix for m in item]
+# print(matrix)
+
+# 29.	Set Disjoint: Check if two different sets of server IP addresses have absolutely no addresses in common.
+# one={1,2,3,4}
+# two={5,6,7,8,4}
+
+# print(one.isdisjoint(two))
+
+# 30.	Symmetric Difference: Find the data points that are in Set A or Set B, but not in both.
+# one={1,2,3,4}
+# two={5,6,7,8,4}
+# print(one.symmetric_difference(two))

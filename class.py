cdd72a973cebae4b76c3b829a6b802afd56d1d93
ahmadcs22 <ahmadsirajsuperior@gmail.com>
2026-04-12 02:10:59 +0500

    classes

---
+++ class.py
@@ -0,0 +1,48 @@
+# Inside the editor, complete the following steps:
+# Create a class called Dog
+# Add an __init__ method with parameters name and age, and store them as properties using self
+# Add a method called bark that prints the dog's name followed by " says Woof!"
+# Create an object d1 of the Dog class with name "Buddy" and age 3
+# Call the bark method on d1
+
+# class dog:
+#     def __init__(self,name,age):
+#         self.name=name
+#         self.age=age
+#     def bark(self):
+#         # print(f'{self.name} woof!')
+#         return f'{self.name} woof!'
+
+# ash = dog("ash",5)
+
+# print(ash.bark())
+
+
+# class hello():
+#     def __init__(self,name):
+#         self.name=name
+#     def greet(self):
+#         return f'hi welcome {self.name}'
+#     def welcome(self):
+#         msg = self.greet()
+#         print('HI there',msg)
+
+# p1=hello("ahmad")
+
+# p1.welcome()
+
+
+# Create a class called Car
+# Add an __init__ method with a brand parameter, and store it as a property
+# Add a method called show that prints the brand
+# Create an object c1 of the Car class with brand "Ford"
+# Call the show method on c1
+
+# class Car():
+#     def __init__(self,name):
+#         self.name=name
+#     def brand(self):
+#         return self.name
+    
+# c1=Car('Toyota')
+# print(c1.brand())

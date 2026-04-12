efa43906cd8d7b798a2be7056e28f31e16d36654
ahmadcs22 <ahmadsirajsuperior@gmail.com>
2026-04-13 01:41:24 +0500

    polymorphism

---
+++ polymorphism.py
@@ -0,0 +1,26 @@
+class car():
+    def __init__(self,brand,model):
+        self.brand=brand 
+        self.model=model
+    def drive(self):
+        return f'{self.brand} {self.model} is driving'
+
+class bike(car):
+    def drive(self):
+        return f'{self.brand} {self.model} is riding'
+
+class boat(car):
+    def drive(self):
+        return f'{self.brand} {self.model} is floating'
+
+
+    
+
+car1=car('toyota','grande')
+bike1=bike('honda',125)
+boat1=boat('boat',67)
+    
+print(car1.drive())
+print(bike1.drive())
+print(boat1.drive())
+

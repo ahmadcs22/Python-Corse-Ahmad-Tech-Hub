6d07e7f83357d38e5a168ec0ccacf5fec8a36127
ahmadcs22 <ahmadsirajsuperior@gmail.com>
2026-04-14 02:31:26 +0500

    functions basic done

--- functions.py
+++ functions.py
@@ -1,2 +1,24 @@
 # Functions (Write Cleaner Code)
 # o	Content: The def keyword. Parameters vs. Arguments. The return statement.
+
+
+# def sum(a,b):
+#     print(a+b)
+
+# sum(10,20)
+
+# f'{}'
+# def student(name,dept,uni='uog'):
+#     print(f'{name} is from {dept} of uni {uni}')
+
+# student(uni='uog',name='ahmad',dept='cs')
+
+
+# positional / keyword based
+
+def add(a,b,c,/):
+    return a+b+c
+
+print(add(10,29,30))
+
+

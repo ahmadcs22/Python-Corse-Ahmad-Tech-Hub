fe3fea6f2f4e54eb468447a0f375169b85b378bc
ahmadcs22 <ahmadsirajsuperior@gmail.com>
2026-06-01 21:51:14 +0500

    numpy basic

---
+++ numpyjee.py
@@ -0,0 +1,19 @@
+import numpy as np 
+
+arr = np.arange(1,11)
+
+arr1= arr + arr + arr
+
+arr2= arr * arr 
+
+arr3= arr / arr
+
+arr4= arr - arr
+
+arr5= arr ** 4
+
+print(f'arr 1 = {arr1}')
+print(f'arr 2 = {arr2}')
+print(f'arr 3 = {arr3}')
+print(f'arr 4 = {arr4}')
+print(f'arr 5 = {arr5}')

b47eef23b50360d44982d06ffbb55c9f1a5febb8
ahmadcs22 <ahmadsirajsuperior@gmail.com>
2026-06-02 02:24:54 +0500

    numpy jee

--- numpyjee.py
+++ numpyjee.py
@@ -1,19 +1,43 @@
 import numpy as np 
 
-arr = np.arange(1,11)
+# arr = np.arange(1,11)
+# hello = np.array([1,2,3,4,5])
 
-arr1= arr + arr + arr
+# arr1= arr + arr + arr
 
-arr2= arr * arr 
+# arr2= arr * arr 
 
-arr3= arr / arr
+# arr3= arr / arr
 
-arr4= arr - arr
+# arr4= arr - arr
 
-arr5= arr ** 4
+# arr5= arr ** 4
 
-print(f'arr 1 = {arr1}')
-print(f'arr 2 = {arr2}')
-print(f'arr 3 = {arr3}')
-print(f'arr 4 = {arr4}')
-print(f'arr 5 = {arr5}')
+# print(f'arr 1 = {arr1}')
+# print(f'arr 2 = {arr2}')
+# print(f'arr 3 = {arr3}')
+# print(f'arr 4 = {arr4}')
+# print(f'arr 5 = {arr5}')
+
+
+# hello = np.sqrt(arr)
+# hello = np.exp(arr)
+# hello = np.max(arr)
+# hello = np.min(arr)
+
+# print(hello)
+# print(np.__version__)
+
+# array2=np.array([1,2,3,4,5],ndmin=5)
+
+# print(array2.ndim)
+
+arr = np.arange(1,13)
+# print(arr)
+# print(arr.reshape(2,3))
+
+print(arr.reshape(4,3))
+print()
+print(arr.reshape(2,6))
+print()
+print(arr.reshape(3,4))

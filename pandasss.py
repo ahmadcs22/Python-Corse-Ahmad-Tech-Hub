8ddd79c56e01635d0dcb0abe0a039c307b88f8c0
ahmadcs22 <ahmadsirajsuperior@gmail.com>
2026-06-03 02:12:56 +0500

    some data set practices

--- pandasss.py
+++ pandasss.py
@@ -22,12 +22,32 @@ import pandas as pd
 
 # print(series1+series2)
 
-np.random.seed(101)
+# np.random.seed(101)
 
-df = pd.DataFrame(
-    np.random.rand(5, 4),
-    index="A B C D E".split(),
-    columns="V W X Y".split()
-)
+# df = pd.DataFrame(
+#     np.random.rand(5, 4),
+#     index="A B C D E".split(),
+#     columns="V W X Y".split()
+# )
+# # df['new']= df['V']+ df['W']
+# # df.drop('new',axis=1)
+# # df.drop(index='E', inplace=True)
+# print(df)
 
+
+data = {
+    'name' : ['ahmad','ali','anaya'],
+    'grades': ['A','B','C']
+}
+
+df = pd.DataFrame(data)
+
+df['lable']= ['97','98','99']
+# df.set_index('lable',drop=False)
+# df=df.reset_index()
+# df.drop('index',index= 1,inplace=True)
+
+df=df.set_index('lable')
+df=df.reset_index()
+df.drop('lable',axis=1,inplace=True)
 print(df)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,1 @@
-t(student.items())
+lable

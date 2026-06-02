34b0bbad76f780aead5cad5454f9e1bc4841717f
ahmadcs22 <ahmadsirajsuperior@gmail.com>
2026-06-02 16:19:20 +0500

    data frames

--- pandasss.py
+++ pandasss.py
@@ -1,22 +1,33 @@
 import numpy as np
 import pandas as pd 
 
-# label = ['ahmad','ali','anaya','hania']
-# arr=[90,91,92,92]
-# datag=np.array([12,20,22,45])
-# di = {
-#     'a': 11,
-#     'b': 22,
-#     'c': 33,
-#     'd': 44
-# }
 
-# hi= pd.Series(arr,di)
-# print(hi)
+# # label = ['ahmad','ali','anaya','hania']
+# # arr=[90,91,92,92]
+# # datag=np.array([12,20,22,45])
+# # di = {
+# #     'a': 11,
+# #     'b': 22,
+# #     'c': 33,
+# #     'd': 44
+# # }
 
-series1 = pd.Series([1,2,3,4,5],index=['psl','ipl','bbl','ilt20','cpl'])
+# # hi= pd.Series(arr,di)
+# # print(hi)
 
-series2=pd.Series([1,2,3,6,5],index=['psl','ipl','bbl','BBB','cpl'] )
+# series1 = pd.Series([1,2,3,4,5],index=['psl','ipl','bbl','ilt20','cpl'])
+
+# series2=pd.Series([1,2,3,6,5],index=['psl','ipl','bbl','BBB','cpl'] )
 
 
-print(series1+series2)
+# print(series1+series2)
+
+np.random.seed(101)
+
+df = pd.DataFrame(
+    np.random.rand(5, 4),
+    index="A B C D E".split(),
+    columns="V W X Y".split()
+)
+
+print(df)

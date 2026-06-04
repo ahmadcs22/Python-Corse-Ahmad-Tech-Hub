98ddb44eb2da8ef7f784a11ba11bda527c0382c6
ahmadcs22 <ahmadsirajsuperior@gmail.com>
2026-06-05 02:46:52 +0500

    fill na and missing vlaue handling

---
+++ missing_values.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+
+# df = pd.DataFrame({
+#     'Name': ['Ahmad', 'Ali', 'Anaya', 'Hania'],
+#     'Marks': [90, np.nan, 85, np.nan]
+# })
+# df.dropna(axis=1,how='all',inplace=True)
+# df.dropna(axis=1,thresh=3,inplace=True)
+# df.dropna(axis=0,subset=['Marks'],inplace=True)
+# df['Marks'].fillna(df['Marks'].mean())
+# df.fillna(method='ffill')
+# df.fillna(method='bfill')
+
+df = pd.DataFrame({
+    'Marks': [90, np.nan, 80]
+})
+
+df['Marks'] = df['Marks'].fillna(df['Marks'].mean())
+
+print(df)
+
+# print(df.isnull().sum())

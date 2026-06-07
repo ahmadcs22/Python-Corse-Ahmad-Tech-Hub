fc0a272958850a89f7810a9e5b875117fefd69a7
ahmadcs22 <ahmadsirajsuperior@gmail.com>
2026-06-07 22:29:21 +0500

    added comment

--- groupbyds.py
+++ groupbyds.py
@@ -1,16 +1,16 @@
 import pandas as pd
 
-# df = pd.DataFrame({
-#     'Team': ['CSK', 'MI', 'CSK', 'MI', 'RCB'],
-#     'Runs': [100, 120, 150, 130, 110]
-# })
-
-# df_new=df.groupby('Team')['Runs'].sum()
+df = pd.DataFrame({
+    'Team': ['CSK', 'MI', 'CSK', 'MI', 'RCB'],
+    'Runs': [100, 120, 150, 130, 110]
+})
+df_new=df.groupby('Team')['Runs'].sum()
 print(df_new)
+
 df = pd.DataFrame({
     'Department':['IT','HR','IT','HR','IT'],
     'Salary':[50000,40000,70000,45000,60000]
 })
 
-new=df.groupby('Department')['Salary'].mean()
+new=df.groupby('Department')['Salary'].median()
 print(new)

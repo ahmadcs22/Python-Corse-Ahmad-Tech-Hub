4c4c7bb2f9d11cda2435f7e87f2e1cdbdd52023e
ahmadcs22 <ahmadsirajsuperior@gmail.com>
2026-06-21 19:04:41 +0500

    numpy oractice

---
+++ numpyassign.py
@@ -0,0 +1,23 @@
+import numpy as np 
+
+# array = np.zeros(10)
+# array = np.ones(10)
+# array = np.ones(10)*5
+# array = np.arange(10,51)
+# array = np.arange(10,51,2)
+# array = np.arange(0,9).reshape(3,3)
+# array = np.eye(3)
+# array = np.random.randint(2,5)
+# array =  np.random.rand(3)
+# array = np.random.randint(1,5,(5,5))
+# np.random.seed(101)
+
+# array = np.random.randint(1,5,(3,3))
+np.random.seed(101)
+# array = np.random.randn(5,5)
+
+# array = np.random.rand(5)
+# array = np.arange(1,101).reshape(10,10)/100
+array = np.linspace(0,1,20)
+
+print(array)

d490ee33a7821939352bd7cb08bc9e455561a82b
ahmadcs22 <ahmadsirajsuperior@gmail.com>
2026-06-22 15:29:41 +0500

    numpy practice

--- numpyassign.py
+++ numpyassign.py
@@ -12,12 +12,68 @@ import numpy as np
 # array = np.random.randint(1,5,(5,5))
 # np.random.seed(101)
 
-# array = np.random.randint(1,5,(3,3))
-np.random.seed(101)
-# array = np.random.randn(5,5)
+# # array = np.random.randint(1,5,(3,3))
+# np.random.seed(101)
+# # array = np.random.randn(5,5)
 
-# array = np.random.rand(5)
-# array = np.arange(1,101).reshape(10,10)/100
-array = np.linspace(0,1,20)
+# # array = np.random.rand(5)
+# # array = np.arange(1,101).reshape(10,10)/100
+# array = np.linspace(0,1,20)
 
-print(array)
+
+
+# array = np.zeros(10)
+# array = np.ones(10)
+# array = np.ones(10)*5
+# array = np.arange(1,51)
+# array = np.arange(10,51,2)
+# array = np.arange(0,9).reshape(3,3)
+# array = np.eye(3)
+# np.random.seed(101)
+
+# # array =np.random.rand(1)
+# # array = np.random.randn(25)
+# # array = np.arange(1,101).reshape(10,10)/100
+# # array = np.linspace(0,1,20)
+
+# print(array)
+
+# arr1= [10, 20, 30, 40, 50]
+
+# arrayjee=np.array(arr1)
+
+
+# arrayjee=np.zeros(15)
+# arrayjee = np.ones((4,4))
+# arrayjee=np.full(7,5)
+# arrayjee=np.eye(5,5)
+# arrayjee= np.arange(0,9).reshape(3,3)
+# arrayjee=np.linspace(1,100,11)
+# arrayjee=np.random.randint(1,10,10)
+# arrayjee=np.random.randn(9).reshape(3,3)
+# np.random.seed(101)
+# Q11 one generates and the othet does is make 10 break points
+# arrayjee=np.arange(1,10)
+# arrayjee.reshape(3,3)
+# it will work bcz it has exaclty nine elements
+# arrayjee=np.arange(16).reshape(4,4)
+# print(arrayjee.shape)
+# bcz it has shape 4x4 with 16 elements 
+# arrayjee = np.array([10,20,30,40,50])
+# print(arrayjee.mean())====30
+
+# arr = np.array([5,4,3,2,1])
+# result = np.std(arr)
+# print(result)
+
+
+# array = np.arange(24).reshape(2,3,4)
+# arr = np.array([10,20,30,40,50])
+
+# print(arr[1:4])
+
+# print(array)
+
+arr = np.array([49, 50, 51, 50, 50])
+
+print(arr.std())

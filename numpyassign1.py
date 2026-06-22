ccb8975a19a3aa22897a9625cd934bbe1b6c29a0
ahmadcs22 <ahmadsirajsuperior@gmail.com>
2026-06-22 23:14:19 +0500

    done with numpy

--- numpyassign1.py
+++ numpyassign1.py
@@ -16,7 +16,7 @@ import numpy as np
 #  [11 12 13 14 15]
 #  [16 17 18 19 20]
 #  [21 22 23 24 25]]
-array = np.arange(1,26).reshape(5,5)
+# array = np.arange(1,26).reshape(5,5)
 
 
 
@@ -25,6 +25,69 @@ array = np.arange(1,26).reshape(5,5)
 # print(array[1:3,1:2])
 # print(array[4])
 # print(array[3:])
-print(array.sum())
-print(array.std())
-print(array.sum(axis=0))
+# print(array.sum())
+# print(array.std())
+# print(array.sum(axis=0))
+
+# Q1
+# a = [5,10,15,20,25,30]
+# array = np.array(a)
+# # print(array)
+# Q2
+# array=np.zeros(16).reshape(4,4)
+# print(array)
+# Q3
+# array = np.ones(9).reshape(3,3)*9
+# print(array)
+# Q4
+# array = np.eye(6)
+# print(array)
+# Q5
+# array = np.arange(1,13).reshape(3,4)
+# print(array)
+# mat = np.arange(1,26).reshape(5,5)
+# [[ 1  2  3  4  5]
+#  [ 6  7  8  9 10]
+#  [11 12 13 14 15]
+#  [16 17 18 19 20]
+#  [21 22 23 24 25]]
+# Q6
+# print(mat[2,2])
+# Q7
+# print(mat[2])
+# Q8
+# print(mat[1:3,1:3])
+# Q9
+# print(mat[3:5,3:5])
+# Q10
+# num = np.random.randint(50,100,20)
+# print(num)
+# Q11
+# num=np.random.randn(16).reshape(4,4)
+# print(num)
+# Q12
+# np.random.seed(42)
+# num = np.random.rand()
+# print(num)
+# arr = np.array([10,20,30,40,50])
+# Q13
+# print(arr.mean())
+# Q14
+# print(arr.std())
+# Q15
+# print(arr.var())
+# arr = np.arange(1,101)
+# Q16
+# print(arr.max())
+# Q17
+# print(arr.min())
+# Q18
+# print(arr.sum())
+# Q19
+# arr = np.arange(1,101).reshape(10,10)
+# print(arr)
+# Q20
+# arr=np.arange(1,101).reshape(10,10)
+# print(arr.sum(axis=1))
+
+

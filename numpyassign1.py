f1438b818026ee3db80f7dcdf8ca22652443e317
ahmadcs22 <ahmadsirajsuperior@gmail.com>
2026-06-26 01:50:16 +0500

    final practice

--- numpyassign1.py
+++ numpyassign1.py
@@ -91,3 +91,23 @@ import numpy as np
 # print(arr.sum(axis=1))
 
 
+
+# array = np.zeros(10)
+# array = np.ones(10)
+# array = np.ones(10)*5
+# array = np.arange(10,51)
+# array = np.arange(10,51,2)
+# array = np.arange(0,9).reshape(3,3)
+# array = np.eye(3)
+
+# array = np.random.rand()
+# array = np.random.randn(25)
+# array = np.arange(1,101)/100
+# array = np.linspace(0,1,20)
+mat = np.arange(1,26).reshape(5,5)
+print(mat)
+# print(mat[2:,1:])
+# print(mat[3,4])
+
+# print(mat[1:3,1:2])
+# print(np.sum(mat))

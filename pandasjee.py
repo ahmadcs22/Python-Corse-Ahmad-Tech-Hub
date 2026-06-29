177286a10fe565f8121897ca68ba3f65a6d608eb
ahmadcs22 <ahmadsirajsuperior@gmail.com>
2026-06-29 22:46:03 +0500

    practice times 2

--- pandasjee.py
+++ pandasjee.py
@@ -1,6 +1,6 @@
-import pandas as pd 
+# import pandas as pd 
 
-df = pd.read_csv('Ecommerce_Purchases.csv')
+# df = pd.read_csv('Ecommerce_Purchases.csv')
 
 # print(df)
 # print(df.head()) 5 row 14 coloms
@@ -19,5 +19,29 @@ df = pd.read_csv('Ecommerce_Purchases.csv')
 # print(df[(df['CC Provider']== 'American Express') & (df['Purchase Price'] >95)].shape[0])
 # print((df['CC Exp Date'].str.split('-').str[2]=='22').sum())
 # print(df['Email'].str.split('@').str[1].value_counts().head(5))
+# ---------------------
 
+
+import pandas as pd 
+
+df = pd.read_csv('Ecommerce_Purchases.csv')
+
+# print(df.head())
+# print(df.shape[1])
+# print(df.shape)
+# print(df['Purchase Price'].mean())
+# print(df['Purchase Price'].max())
+# print(df['Purchase Price'].min())
+# print(df[df['Language']=='en'].shape[0])
+# print(df[df['Job']== 'Lawyer'].shape[0])
+# print(df[df['AM or PM']== 'AM'].shape[0])
+# print(df[df['AM or PM']== 'PM'].shape[0])
+# print(df['Job'].value_counts().head(5))
+# print(df.loc[ df['Lot']== '90 WT',"Purchase Price"].values[0])
+# print(df.loc[df['Credit Card']== 6011580000000000,"Email"].values[0])
+# print(df[(df["CC Provider"]== "American Express") & (df["Purchase Price"] > 95)].shape[0])
+# print((df['CC Exp Date'].str.split('-').str[2] == '22').sum())
+# print((df['Email'].str.split('@').str[1].value_counts().head(5)))
+
+ 
 
